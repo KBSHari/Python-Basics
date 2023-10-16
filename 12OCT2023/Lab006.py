7b0f0953db843f470917fa10c228106b0eaabe61
KBSHari <93704459+KBSHari@users.noreply.github.com>
2023-10-16 06:54:00 +0530

    Add files via upload

---
+++ 12OCT2023/Lab006.py
@@ -0,0 +1,24 @@
+#Task #1
+
+#Explain the difference between the = operator and the == operator in Python.
+
+"""(=) is a Assignment Operators, for assigning a value to a variable. a=12 ,b=13
+(==) is a Relational Operators, checks the relationship between two operands [a==b] return bool(True & False)"""
+
+#What does the ** operator do in Python, and how is it used?
+
+#We call Exponentiation or power operator x**y
+
+a=2
+b=5
+print(2**5) #(2*2*2*2*2)
+
+#What does the ^ operator do in Python, and in what context is it commonly used?"""
+#xor operation 0 0=0,1 0=1,0 1=1,1 1=0
+
+"""25=0001 1001
+^
+30=0001 1110
+=0000 0111"""
+
+print(25^30)
